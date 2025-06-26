497ff76288c0933fa12bab7d00a26fad8586d19c
Kippour <juniorkippour@gmail.com>
2025-06-25 21:42:27 -0400

    First submission.

---
+++ P1LAB1_SembeneAdama.py
@@ -0,0 +1,11 @@
+# Adama Sembene
+# June 19, 2025
+# P1LAB1
+# This program asks the user for their first and last name and displays a welcome message.
+
+# Get the user's first and last name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display the welcome message
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
